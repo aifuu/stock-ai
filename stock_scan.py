1cf8c93eacf58809b27ed9e296d886ec8432ca8a
aifuu <ttaiki121121@gmail.com>
2026-08-07 23:09:18 +0900

    Update stock_scan.py

--- stock_scan.py
+++ stock_scan.py
@@ -29,17 +29,17 @@ def check_prediction_history():
         try:
             
             print(row["ticker"], row["date"])
-
-        df = yf.download(
-            row["ticker"],
-            period="10d",
-            interval="1d",
-            auto_adjust=True
-        )
-
-        if len(df) < 4:
-            print("データ不足")
-            continue
+            
+            df = yf.download(
+                row["ticker"],
+                period="10d",
+                interval="1d",
+                auto_adjust=True
+            )
+            
+            if len(df) < 4:
+                print("データ不足")
+                continue
 
 
         take_profit = float(row["take_profit"])
